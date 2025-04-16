6803f84200b2784111baf3ed8c7b000ab5396f9e
unknown <herreracyjay24@gmail.com>
2025-04-16 22:44:05 +0800

    feat: signin and register, along with likes

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,66 @@
 "use client";
-
 import Header from "./ui/header";
 import Post from "./ui/post";
 import { useState, useEffect } from 'react';
 import { todayPosts } from "./lib/supabase";
 import { Posts } from './lib/definitions';
 import { supabase } from './lib/supabase';
+import { User } from "@supabase/supabase-js";
 
 export default function Home() {
+  const [user, setUser] = useState<User | null>(null);
+  const [username, setUsername] = useState('');
   const [post, setPost] = useState<Posts[]>([]);
   const [createPostText, setCreatePostText] = useState<string>("");
+  const [loadingWidth, setLoadingWidth] = useState('0%');
+
   useEffect(() => {
     const postsToday = async () => {
+      setLoadingWidth('30%');
       const data = await todayPosts();
       setPost(data);
+      stopLoader();
     };
+    const getUser = async () => {
+      const { data, error } = await supabase.auth.getUser();
+      if (data?.user) {
+        setUser(data.user);
+        const { data:userData } = await supabase 
+            .from('users')
+            .select('*')
+            .eq('id', data.user.id)
+            .single();
+        if(userData){
+            setUsername(userData.username);
+        }
+      }
+    };
+
+    getUser();
     postsToday();
   }, []);
 
+  const startLoader:any = function(){
+    setLoadingWidth('30%');
+  }
+  const stopLoader: any = function(){
+    setLoadingWidth('100%');
+    // Reset after delay
+    setTimeout(() => {
+        setLoadingWidth('0%');
+    }, 600);
+  }
+
   const createPost = async () => {
+    setLoadingWidth('30%');
     const { data, error } = await supabase
       .from('posts')
       .insert([
         {
           post_text: createPostText,
+          like_amount: 0,
+          sender_id: (user) ? user.id : null,
+          comments_amount: 0
         }
       ])
       .select();
@@ -34,32 +71,38 @@ export default function Home() {
       setPost((prevPosts) => [data[0], ...prevPosts]);
       setCreatePostText('');
     }
+    stopLoader();
   };
   
   return (
-    <div className="flex h-screen w-screen bg-[#F4F4F8] text-black relative">
+    <div className="flex min-h-screen bg-[#F4F4F8] text-black dark:bg-black pb-5">
+      <div id="loading-indicator" style={{
+            width: loadingWidth,
+            transition: 'width 0.3s ease-in-out'
+          }} 
+          className="w-full h-2 bg-green-500 absolute top-0"></div>
       <div className="px-10 w-full flex flex-col gap-5 ">
-        <Header />
+        <Header username={username}/>
         <div className="flex gap-10">
           <main className="flex flex-col gap-5 w-3/5">
             {
               post.map((post, index) => (
-                <Post key={index} post={post}/>
+                <Post key={index} post={post} startLoader={startLoader} stopLoader={stopLoader}/>
               ))
             }
           </main>
           <aside className="flex flex-col w-2/5 h-min">
-            <div className="flex flex-col gap-4 w-full bg-white rounded-lg shadow-sm px-4 py-3 text-[13px]">
-              <p className="">Anonymous</p>
+            <div className="flex flex-col gap-4 w-full bg-white rounded-lg shadow-sm px-4 py-3 text-[13px] dark:bg-[#1A1A40] dark:text-white">
+              <p className="">{username? `@${username}` : 'Anonymous'}</p>
               <textarea 
                 rows={6}
                 value={createPostText} 
                 onChange={(e) => setCreatePostText(e.target.value)} 
-                className="px-3 py-2 border border-gray-300 rounded-lg focus:outline-none" placeholder="What's on your mind?" />
+                className="px-3 py-2 border border-gray-300 rounded-lg focus:outline-none dark:bg-[#1A1A40]" placeholder="What's on your mind?" />
               <div className="w-full flex justify-end">
                 <button 
                   onClick={createPost} 
-                  className="px-4 py-1.5 bg-black text-white rounded-lg">Post</button>
+                  className="px-4 py-1.5 bg-black text-white rounded-lg dark:bg-white dark:text-black">Post</button>
               </div>
             </div>
           </aside>
--- app/ui/header.tsx
+++ app/ui/header.tsx
@@ -1,9 +1,28 @@
 "use client";
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
 
-const Header: React.FC = () => {
+const Header = ({ username }: { username: string }) => {
+    useEffect(() => {
+        const isDark = localStorage.getItem('theme') === 'dark';
+        isDark ? document.documentElement.classList.add('dark') : document.documentElement.classList.remove('dark');
+    }, []);
+
+    const toggleDarkMode = () => {
+        if (typeof window !== 'undefined') {
+        const isDark = document.documentElement.classList.contains('dark');
+            if (isDark) {
+                document.documentElement.classList.remove('dark');
+                localStorage.setItem('theme', 'light');
+            } else {
+                document.documentElement.classList.add('dark');
+                localStorage.setItem('theme', 'dark');
+            }
+        }
+    };
+
     return (
-        <div className='flex justify-between py-2 pt-4 text-black items-center'>
+        <div className='flex justify-between py-2 pt-4 text-black items-center dark:text-white'>
             {/* LINKS */}
             <div className="flex gap-4 items-center ">
                 <a href="/  " className="hover:text-blue-800" >All</a>
@@ -11,10 +30,16 @@ const Header: React.FC = () => {
                 <a href="/analytics" className="hover:text-blue-800">Analytics</a>
             </div>
             {/* Profile */}
+            <button onClick={toggleDarkMode}>🌓 Toggle Dark Mode</button>
             <button className="flex items-center rounded-full py-1 h-min border border-gray-300 text-sm pl-2 pr-3 gap-2 ">
-                <div className="h-6 w-6 rounded-full bg-black"></div>
+                <div className="h-6 w-6 rounded-full bg-black dark:bg-white"></div>
                 <span>
-                    <a className="hover:text-blue-800">Login</a> /  <a className="hover:text-blue-800">Register</a>
+                    {
+                        username ? (
+                            <p>{username}</p>) : ( <>
+                            <Link href="/signin" className="hover:text-blue-800">Login</Link> /  <Link href="/register" className="hover:text-blue-800">Register</Link></>
+                        )
+                    }
                 </span>
             </button>
         </div>
