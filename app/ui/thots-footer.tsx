8754b5cd1b205f5e22d8013b6a2760422dbbeb16
Perseus24 <herreracyjay24@gmail.com>
2025-06-10 23:37:24 +0800

    chore: added link for the project tracker app

--- app/ui/thots-footer.tsx
+++ app/ui/thots-footer.tsx
@@ -9,6 +9,7 @@ const ThotsFooter = () => {
                 <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
                     <li>
                         <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
+                        <a href="https://projectrackr.vercel.app/" className="mr-4 hover:underline md:mr-6 ">ProjectTrackr</a>
                     </li>
                     <li>
                         <a href="/developer" className="mr-4 hover:underline md:mr-6">{"Developer's Profile"}</a>
